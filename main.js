f172e75e141ca4fe754a5bbdf90a941b95dae7d1
meguroryo <ryo2219-dio@ezweb.ne.jp>
2020-08-25 23:41:23 +0900

    mygame1-v1.1

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 
 let cvs_width = 912;
 let cvs_height = 624;
-let field_width=8;
-let field_height=8;
+let field_width = 8;
+let field_height = 8;
 
 let field_left = 24;
 let field_up = 24;
@@ -26,10 +26,10 @@ field = [
 
 var click_x = -1, click_y = -1, click_flg = false;
 
-function make_field(){
-    for(var y=3;y<8;y++){
-        for(var x=0;x<8;x++){
-            field[y][x]=randint(1,8);
+function make_field() {
+    for (var y = 3; y < 8; y++) {
+        for (var x = 0; x < 8; x++) {
+            field[y][x] = randint(2, 8);
         }
     }
 }
@@ -38,13 +38,13 @@ window.addEventListener("click", function () {
     let px = event.clientX;
     let py = event.clientY;
 
-    let obj = document.elementFromPoint(px,py);
+    let obj = document.elementFromPoint(px, py);
     let objx = obj.getBoundingClientRect().left;
     let objy = obj.getBoundingClientRect().top;
 
-    click_x=px-objx;
-    click_y=py-objy;
-    click_flg=1;
+    click_x = px - objx;
+    click_y = py - objy;
+    click_flg = 1;
 });
 
 window.addEventListener('load', function () {
@@ -55,8 +55,8 @@ function logPosition(event) {
 
     if (event.changedTouches[0]) {
         // ページ
-        let px=event.changedTouches[0].clientX;
-        let py=event.changedTouches[0].clientY;
+        let px = event.changedTouches[0].clientX;
+        let py = event.changedTouches[0].clientY;
 
         let obj = document.elementFromPoint(px, py);
         let objx = obj.getBoundingClientRect().left;
@@ -69,11 +69,11 @@ function logPosition(event) {
 }
 
 
-function display_message(){
-    window.alert("縦・横・斜めに3つブロックを揃えると消える\n2行目より上のブロックを全て消せばクリア\nクリック回数が少ないほどランクが高くなる");
+function display_message() {
+    window.alert("縦・横・斜めに3つブロックを揃えると消える\n2行目より上のブロックを全て消せばクリア\nクリック回数が少ないほどランクが高くなる\n\n追記\n赤色のブロックは爆弾ブロックに変更(落ちたところの周囲のブロックを消す)\nrankを修正(最初のブロックの個数に依存するようになった)\nバグ見つけたらツイッターでリプ下さい,修正します");
 }
 
-function draw_frame(){
+function draw_frame() {
     context.fillStyle = "white";
     for (var y = 0; y <= field_width; y++) {
         context.fillRect(field_left + y * 72, field_up, 1, field_height * 72);
@@ -83,7 +83,7 @@ function draw_frame(){
     }
 }
 
-function draw_block(){
+function draw_block() {
     delete_block();
     for (var y = 0; y < field_height; y++) {
         for (var x = 0; x < field_width; x++) {
@@ -95,7 +95,7 @@ function draw_block(){
     }
 }
 
-function decide_color(color_number){
+function decide_color(color_number) {
     if (color_number == 1) {
         context.fillStyle = "red";
     } else if (color_number == 2) {
@@ -110,7 +110,7 @@ function decide_color(color_number){
         context.fillStyle = "white";
     } else if (color_number == 7) {
         context.fillStyle = "gray";
-    } else if (color_number == 8){
+    } else if (color_number == 8) {
         context.fillStyle = "skyblue";
     }
 }
@@ -129,14 +129,14 @@ function randint(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function fall_block(){
-    var flag=false;
-    for(var y=6;y>=0;y--){
-        for(var x=0;x<8;x++){
-            if(field[y][x]!=0 && field[y+1][x]==0){
-                field[y+1][x]=field[y][x];
-                field[y][x]=0;
-                flag=true;
+function fall_block() {
+    var flag = false;
+    for (var y = 6; y >= 0; y--) {
+        for (var x = 0; x < 8; x++) {
+            if (field[y][x] != 0 && field[y + 1][x] == 0) {
+                field[y + 1][x] = field[y][x];
+                field[y][x] = 0;
+                flag = true;
             }
         }
     }
@@ -144,12 +144,12 @@ function fall_block(){
     return flag;
 }
 
-function sweep_block(){
-    var cnt=0;
-    for(var y=0;y<8;y++){
-        for(var x=0;x<8;x++){
-            if(field[y][x]==8){
-                field[y][x]=0;
+function sweep_block() {
+    var cnt = 0;
+    for (var y = 0; y < 8; y++) {
+        for (var x = 0; x < 8; x++) {
+            if (field[y][x] == 8) {
+                field[y][x] = 0;
                 cnt++;
             }
         }
@@ -158,7 +158,7 @@ function sweep_block(){
     return cnt;
 }
 
-var ins=[
+var ins = [
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -170,79 +170,131 @@ var ins=[
 
 ]
 
-function check_three(){
-    for(var y=0;y<8;y++){
-        for(var x=0;x<8;x++){
-            ins[y][x]=field[y][x];
+function in_boxes(v_y,v_x){
+    if(0<=v_y && v_y<8 && 0<=v_x && v_x<8){
+        return true;
+    }
+    return false;
+}
+
+function check_three() {
+    for (var y = 0; y < 8; y++) {
+        for (var x = 0; x < 8; x++) {
+            ins[y][x] = field[y][x];
         }
     }
-    for(var y=1;y<7;y++){
-        for(var x=0;x<8;x++){
-            if(ins[y][x]!=0){
-                if(ins[y-1][x]==ins[y][x] && ins[y][x]==ins[y+1][x]){
-                    field[y-1][x]=field[y][x]=field[y+1][x]=8;
+    for (var y = 1; y < 7; y++) {
+        for (var x = 0; x < 8; x++) {
+            if (ins[y][x] != 0) {
+                if (ins[y - 1][x] == ins[y][x] && ins[y][x] == ins[y + 1][x]) {
+                    field[y - 1][x] = field[y][x] = field[y + 1][x] = 8;
                 }
             }
         }
     }
-    for(var y=0;y<8;y++){
-        for(var x=1;x<7;x++){
-            if(ins[y][x]!=0){
-                if(ins[y][x-1]==ins[y][x] && ins[y][x]==ins[y][x+1]){
-                    field[y][x-1]=field[y][x]=field[y][x+1]=8;
+    for (var y = 0; y < 8; y++) {
+        for (var x = 1; x < 7; x++) {
+            if (ins[y][x] != 0) {
+                if (ins[y][x - 1] == ins[y][x] && ins[y][x] == ins[y][x + 1]) {
+                    field[y][x - 1] = field[y][x] = field[y][x + 1] = 8;
                 }
             }
         }
     }
-    for(var y=1;y<7;y++){
-        for(var x=1;x<7;x++){
-            if(ins[y][x]!=0){
-                if(ins[y-1][x-1]==ins[y][x] && ins[y][x]==ins[y+1][x+1]){
-                    field[y-1][x-1]=field[y][x]=field[y+1][x+1]=8;
+    for (var y = 1; y < 7; y++) {
+        for (var x = 1; x < 7; x++) {
+            if (ins[y][x] != 0) {
+                if (ins[y - 1][x - 1] == ins[y][x] && ins[y][x] == ins[y + 1][x + 1]) {
+                    field[y - 1][x - 1] = field[y][x] = field[y + 1][x + 1] = 8;
                 }
             }
         }
     }
-    for(var y=1;y<7;y++){
-        for(var x=1;x<7;x++){
-            if(ins[y][x]!=0){
-                if(ins[y-1][x+1]==ins[y][x] && ins[y][x]==ins[y+1][x-1]){
-                    field[y-1][x+1]=field[y][x]=field[y+1][x-1]=8;
+    for (var y = 1; y < 7; y++) {
+        for (var x = 1; x < 7; x++) {
+            if (ins[y][x] != 0) {
+                if (ins[y - 1][x + 1] == ins[y][x] && ins[y][x] == ins[y + 1][x - 1]) {
+                    field[y - 1][x + 1] = field[y][x] = field[y + 1][x - 1] = 8;
                 }
+            }
+        }
+    }
+
+    for (var y = 0; y < 8; y++) {
+        for (var x = 0; x < 8; x++) {
+            if (ins[y][x] == 1) {
+                field[y][x] = 8;
+                if (in_boxes(y - 1, x - 1))
+                    if (ins[y - 1][x - 1] != 0)
+                        field[y - 1][x - 1] = 8;
+                if (in_boxes(y - 1, x))
+                    if (ins[y - 1][x] != 0)
+                        field[y - 1][x] = 8;
+                if (in_boxes(y - 1, x + 1))
+                    if (ins[y - 1][x + 1] != 0)
+                        field[y - 1][x + 1] = 8;
+                if (in_boxes(y, x - 1))
+                    if (ins[y][x - 1] != 0)
+                        field[y][x - 1] = 8;
+                if (in_boxes(y, x + 1))
+                    if (ins[y][x + 1] != 0)
+                        field[y][x + 1] = 8;
+                if (in_boxes(y + 1, x - 1))
+                    if (ins[y + 1][x - 1] != 0)
+                        field[y + 1][x - 1] = 8;
+                if (in_boxes(y + 1, x))
+                    if (ins[y + 1][x] != 0)
+                        field[y + 1][x] = 8;
+                if (in_boxes(y + 1, x + 1))
+                    if (ins[y + 1][x + 1] != 0)
+                        field[y + 1][x + 1] = 8;
             }
         }
     }
 }
 
-function game_clear(){
-    var flag=true;
-    for(var x=0;x<8;x++){
-        if(field[5][x]!=0){
-            flag=false;
+function game_clear() {
+    var flag = true;
+    for (var x = 0; x < 8; x++) {
+        if (field[5][x] != 0) {
+            flag = false;
         }
     }
     return flag;
 }
 
-function game_over(){
-    var flag=true;
-    for(var x=0;x<8;x++){
-        if(field[0][x]==0){
-            flag=false;
+function game_over() {
+    var flag = true;
+    for (var x = 0; x < 8; x++) {
+        if (field[0][x] == 0) {
+            flag = false;
         }
     }
     return flag;
 }
 
-var process=2,click_counter=0,next_block=randint(1,8);
-var flag_cnt=true;
-var first_flg=true;
-var time_cnt=0;
+function count_blocks(){
+    var cnt_blc=0;
+    for(var y=0;y<8;y++){
+        for(var x=0;x<8;x++){
+            if(field[y][x]!=0){
+                cnt_blc++;
+            }
+        }
+    }
+    return cnt_blc;
+}
+
+var process = 2, click_counter = 0, next_block = Math.floor((randint(1, 13)+2)/2+0.1);
+var flag_cnt = true;
+var first_flg = true;
+var time_cnt = 0;
+var first_block_cnt=0;
 
 requestAnimationFrame(main);
-function main(){
-    
-    if(time_cnt!=5){
+function main() {
+
+    if (time_cnt != 5) {
         time_cnt++;
     } else {
         time_cnt = 0;
@@ -250,13 +302,13 @@ function main(){
 
         if (first_flg == true) {
             first_flg = false;
+            display_message();
             make_field();
             draw_frame();
             delete_block();
             draw_block();
-
-            display_message();
-        }else{
+            first_block_cnt=count_blocks();
+        } else {
             draw_frame();
             delete_block();
             draw_block();
@@ -267,8 +319,8 @@ function main(){
         context.font = "bold 25pt sans-serif";
         context.textAlign = "center";
         context.fillText("NEXT BLOCK", 752, 24 + 72);
-        context.fillText("COUNT : "+click_counter,752,24+72*5);
-        
+        context.fillText("COUNT : " + click_counter, 752, 24 + 72 * 5);
+
         if (process == 1) {
             if (fall_block() == false) {
                 process = 2;
@@ -326,18 +378,19 @@ function main(){
             context.fillText(string, 24 + 72 * 4, 24 + 72 * 4);
 
             var rank;
-            if(click_counter<=30){
-                rank="S";
-            }else if(click_counter<=50){
-                rank="A";
-            }else if(click_counter<=80){
-                rank="B";
-            }else if(click_counter<=110){
-                rank="C";
-            }else if(click_counter<=140){
-                rank="D";
-            }else{
-                rank="E";
+            var calc = first_block_cnt/2-5;
+            if (click_counter <= calc) {
+                rank = "S";
+            } else if (click_counter <= calc) {
+                rank = "A";
+            } else if (click_counter <= calc+13) {
+                rank = "B";
+            } else if (click_counter <= calc+26) {
+                rank = "C";
+            } else if (click_counter <= calc+39) {
+                rank = "D";
+            } else {
+                rank = "E";
             }
 
             context.fillText("YOUR RANK : " + rank, 24 + 72 * 4, 24 + 72 * 5);
